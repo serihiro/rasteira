ece8cc3d85249b93c7e12544b3e2c94ce89cd9f6
Kazuhiro Serizawa <nserihiro@gmail.com>
2017-12-09 10:34:53 +0900

    introduce rubocop
    bundle exec rubocop --only Style -a
    
    fix a `Style/ClassAndModuleChildren` caution
    
    fix `Style/Documentation` cautions
    
    disable `Metrics/MethodLength`
    
    disbale `HeredocDelimiterNaming`
    
    fix a `Performance/TimesMap` caution

--- spec/embed_worker/manager_spec.rb
+++ spec/embed_worker/manager_spec.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe Rasteira::EmbedWorker::Manager do
   describe '#run' do
     let(:manager) { Rasteira::EmbedWorker::Manager.new }
-    
+
     it 'charges thread pool' do
       expect do
         manager.run
       end.to change { manager.instance_variable_get(:@thread_pool).size }.from(0).to(5)
     end
-    
+
     it 'starts thread_manager thread' do
       manager.run
       thread_manager = manager.instance_variable_get(:@thread_manager)
@@ -19,7 +21,7 @@ RSpec.describe Rasteira::EmbedWorker::Manager do
 
   describe '.run' do
     subject { Rasteira::EmbedWorker::Manager.run }
-    
+
     it 'returns manager instance' do
       expect(subject.class).to eq Rasteira::EmbedWorker::Manager
     end
@@ -33,38 +35,40 @@ RSpec.describe Rasteira::EmbedWorker::Manager do
       expect(thread_manager.alive?).to eq true
     end
   end
-  
+
   describe '#enqueue_job!' do
     before :each do
       # mock
-      class Rasteira::Core::Job
-        def initialize(_worker_name, _options = {})
+      module Rasteira
+        module Core
+          class Job
+            def initialize(_worker_name, _options = {}); end
+          end
         end
       end
     end
     let(:manager) { Rasteira::EmbedWorker::Manager.new }
-    
+
     context 'with worker_path' do
       subject { manager.enqueue_job!('worker_name', worker_path: 'worker_path') }
-  
+
       it 'increase @job_pool count' do
         expect { subject }.to change { manager.job_pool.size }.from(0).to(1)
       end
     end
-    
-    
+
     context 'without worker_path' do
       subject { manager.enqueue_job!('worker_name') }
-  
+
       it 'increase @job_pool count' do
         expect { subject }.to change { manager.job_pool.size }.from(0).to(1)
       end
     end
   end
-  
+
   describe '#shutdown_workers!' do
     let(:manager) { Rasteira::EmbedWorker::Manager.run }
-    
+
     it 'stop all threads of @thread_pool and @thread_manager' do
       manager.shutdown_workers!
       sleep(0.1)
